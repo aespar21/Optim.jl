4972811fd410a5a9915b9a8a190f5096492f022c
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2017-01-23 10:09:51 +0100

    Add tensor tests.

--- test/array.jl
+++ test/array.jl
@@ -13,7 +13,7 @@
             res = optimize(f, g!, [1., 0., 1., 0.], GradientDescent())
 
             @test typeof(Optim.minimizer(res)) <: Vector
-            @test norm(Optim.minimizer(res) - [10.0, 0.0, 0.0, 5.0]) < 10e-8
+            @test vecnorm(Optim.minimizer(res) - [10.0, 0.0, 0.0, 5.0]) < 10e-8
         end
         # TODO: Get finite differencing to work for generic arrays as well
         # optimize(f, eye(2), method = :gradient_descent)
@@ -24,7 +24,21 @@
             res = optimize(f, g!, eye(2), GradientDescent())
 
             @test typeof(Optim.minimizer(res)) <: Matrix
-            @test norm(Optim.minimizer(res) - [10.0 0.0; 0.0 5.0]) < 10e-8
+            @test vecnorm(Optim.minimizer(res) - [10.0 0.0; 0.0 5.0]) < 10e-8
+        end
+        # TODO: Get finite differencing to work for generic arrays as well
+        # optimize(f, eye(2), method = :gradient_descent)
+    end
+
+    @testset "tensor" begin
+        eye3 = Array{Float64,3}(2,2,1)
+        eye3[:,:,1] = eye(2)
+        for m in (AcceleratedGradientDescent(), ConjugateGradient(), BFGS(), LBFGS(), NelderMead(), GradientDescent(), MomentumGradientDescent(), NelderMead(), SimulatedAnnealing(), ParticleSwarm())
+            res = optimize(f, g!, eye3, GradientDescent())
+            _minimizer = Optim.minimizer(res)
+            @test typeof(_minimizer) <: Array
+            @test size(_minimizer) == (2,2,1)
+            @test vecnorm(_minimizer - [10.0 0.0; 0.0 5.0]) < 10e-8
         end
         # TODO: Get finite differencing to work for generic arrays as well
         # optimize(f, eye(2), method = :gradient_descent)
