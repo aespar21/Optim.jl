a1d7f38604906dae02029e3ea218f88157278b82
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2017-01-24 13:13:29 +0100

    fix new tests in bfgs, lbfgs, mgd.

--- test/l_bfgs.jl
+++ test/l_bfgs.jl
@@ -3,7 +3,7 @@
         for (name, prob) in Optim.UnconstrainedProblems.examples
             if prob.isdifferentiable
                 if diff == :analytic
-                    res = Optim.optimize(prob.f, prob.initial_x, LBFGS())
+                    res = Optim.optimize(prob.f, prob.g!, prob.initial_x, LBFGS())
                     @test norm(Optim.minimizer(res) - prob.solutions) < 1e-2
                 else
                     od = OnceDifferentiable(prob.f, prob.initial_x, method = diff)
